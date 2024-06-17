35e2f6bceef7a858903179d13d99b1df986a6e0a
Nello Rizzo <nellorizzo@live.it>
2024-06-17 15:50:03 +0200

    Spunti per l'esercitazione del 17/06.

---
+++ Es1706/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Es1706
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Athlete athlete = new Athlete();
+            athlete.FirstName = "Archimede";
+            athlete.LastName = "Pitagorico";
+            athlete.Sport = "Olimpiadi di Matematica";
+            athlete.ShowMe();
+
+            Employee paperino = new Employee();
+            paperino.FirstName = "Paolino";
+            paperino.LastName = "Paperino";
+            paperino.Id = "17171717";
+            paperino.Salary = 1000;
+            paperino.Department = "Pulizie";
+            paperino.ShowMe();
+
+            Employee topolino = new Employee();
+            topolino.FirstName = "Mickey";
+            topolino.LastName = "Mouse";
+            topolino.Id = "007";
+            topolino.Salary = 10000;
+            topolino.Department = "Investigazioni";
+            topolino.ShowMe();
+        }
+    }
+}
