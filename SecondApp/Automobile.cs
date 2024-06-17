0491bae3f77bcbb813e06af647ba76860cd91ae9
Nello Rizzo <nellorizzo@live.it>
2024-06-17 12:57:52 +0200

    Codice del 17/06

---
+++ SecondApp/Automobile.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondApp
+{
+    // class -> in OOP è il contenitore di un'informazione complessa
+    // cioè composta da tante informazioni elementari ognuna delle quali
+    // rappresenta UN SINGOLO attributo dell'informazione completa
+    internal class Automobile
+    {
+        // campi di un'automobile
+        int velocita;
+        string colore;
+        string tipoAutovettura;
+        int annoImmatricolazione;
+        // Proprietà
+        public int Velocita
+        {
+            // getter
+            get { return velocita; }
+            // setter
+            set { velocita = value; }
+        }
+
+        public int AnnoImmatricolazione
+        {
+            get { return annoImmatricolazione; }
+            set { annoImmatricolazione = value; }
+        }
+    }
+}
---
+++ SecondApp/Calcolatrice.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondApp
+{
+    internal class Calcolatrice
+    {
+        private decimal accumulatore;
+
+        public decimal Accumulatore
+        {
+            get { return accumulatore; }
+        }
+
+        public void Somma(decimal valore)
+        {
+            accumulatore = accumulatore + valore;
+        }
+
+        public decimal AccumulatoreInvertito()
+        {
+            return 1/accumulatore;
+        }
+    }
+}
