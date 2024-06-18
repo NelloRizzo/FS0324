5f1fdc357114191152d2f9547e96e4104a59411f
Nello Rizzo <nellorizzo@live.it>
2024-06-18 16:53:19 +0200

    Esempi vari di inizializzazione di oggetti.

--- U1_D2_Esercizio/Program.cs
+++ U1_D2_Esercizio/Program.cs
@@ -5,13 +5,13 @@
         static void Main(string[] args)
         {
             Person[] people =
-            [
+            {
                 new Person("Paperon","De' Paperoni", 80),
                 new Person("Paperino", "Paolino"),
                 new Person{ Age = 16, FirstName="Qui", LastName = ""},
                 new Person{ Age = 16, FirstName="Quo", LastName = ""},
-                new Person{ Age = 16, FirstName="Qua", LastName = ""},
-            ];
+                new() { Age = 16, FirstName="Qua", LastName = ""},
+            };
             for (int i = 0; i < people.Length; i++)
             {
                 Console.WriteLine(people[i].Description());
