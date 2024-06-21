e6910a44d888f9be96bcdd730e74c4d86d262499
Nello Rizzo <nellorizzo@live.it>
2024-06-21 08:56:15 +0200

    Programma per un contest interno.

---
+++ Contest/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Diagnostics;
+
+namespace Contest
+{
+    internal class Program
+    {
+        /// <summary>
+        /// Stampa tutti i numeri primi da <code>2</code> fino al numero specificato nel parametro <code>upperBound</code>.
+        /// </summary>
+        /// <param name="upperBound">Limite superiore dell'intervallo da considerare per l'estrazione dei numeri primi.</param>
+        private static void Primes(int upperBound)
+        {
+        }
+        static void Main(string[] args)
+        {
+            // un cronometro
+            Stopwatch sw = new Stopwatch();
+            // attiva il cronometro
+            sw.Start();
+            // esegue il metodo da misurare
+            Primes(10000);
+            // ferma il cronometro
+            sw.Stop();
+            // stampa il tempo trascorso
+            Console.WriteLine($"Execution time: {sw.ElapsedMilliseconds} ms");
+        }
+    }
+}
