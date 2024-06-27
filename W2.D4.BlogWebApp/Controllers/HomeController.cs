2f93a13e6221fa910dd695130dc4558615070193
Nello Rizzo <nellorizzo@live.it>
2024-06-27 17:35:46 +0200

    Upload di immagini.

--- W2.D4.BlogWebApp/Controllers/HomeController.cs
+++ W2.D4.BlogWebApp/Controllers/HomeController.cs
@@ -11,13 +11,16 @@ namespace W2.D4.BlogWebApp.Controllers
         private readonly ILogger<HomeController> _logger;
         private readonly IArticleService _articleService;
         private readonly ICommentService _commentService;
+        private readonly IWebHostEnvironment _env;
 
         // articleService è inizializzato tramite DI
-        public HomeController(ILogger<HomeController> logger, IArticleService articleService, ICommentService commentService) {
+        public HomeController(ILogger<HomeController> logger, IArticleService articleService, 
+            ICommentService commentService, IWebHostEnvironment env) {
             _logger = logger;
             // questa variabile consentirà di accedere al servizio all'interno della classe
             _articleService = articleService;
             _commentService = commentService;
+            _env = env;
         }
 
         public IActionResult Index() {
@@ -28,6 +31,26 @@ namespace W2.D4.BlogWebApp.Controllers
         public IActionResult Write() {
             return View(new Article());
         }
+        public IActionResult WriteArticle() {
+            return View(new ArticleInputModel());
+        }
+        [HttpPost]
+        public IActionResult WriteArticle(ArticleInputModel article) {
+            var a = new Article { Content = article.Content, Title = article.Title };
+            _articleService.Create(a);
+            // combina più parti di un path del file system
+            string uploads = Path.Combine(_env.WebRootPath, "images");
+            // Cover è l'immagine in upload
+            if (article.Cover.Length > 0) {
+                // creo il nome del file per il salvataggio
+                string filePath = Path.ChangeExtension(Path.Combine(uploads, a.Id.ToString()), "jpg");
+                // un file stream è un canale per scrivere su un file del file system
+                using Stream fileStream = new FileStream(filePath, FileMode.Create);
+                article.Cover.CopyTo(fileStream);
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
         [HttpPost]
         public IActionResult Write(Article article) {
             _articleService.Create(article);
@@ -38,6 +61,11 @@ namespace W2.D4.BlogWebApp.Controllers
             var article = _articleService.GetById(id);
             var comments = _commentService.GetAll(id);
             ViewBag.Comments = comments;
+            string uploads = Path.Combine(_env.WebRootPath, "images");
+            var cover = Path.ChangeExtension(Path.Combine(uploads, article.Id.ToString()), "jpg");
+            // controllo che il file il cui nome è memorizzato in cover esista realmente
+            if (System.IO.File.Exists(cover))
+                ViewBag.Cover = $"/images/{article.Id}.jpg";
             return View(article);
         }
 
