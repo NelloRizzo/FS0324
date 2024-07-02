2f33a69375d5e3f14289e632526170f7e42f03cd
Nello Rizzo <nellorizzo@live.it>
2024-07-02 13:49:32 +0200

    esempio di select di righe completa

---
+++ W3_D1_Sql/select4.sql
@@ -0,0 +1,14 @@
+﻿select  -- 3
+	denominazione_comune, Superficie_totale_ettari
+from -- 1
+	daticomunali
+where -- 2
+	codice_provincia = 1 or codice_provincia = 2
+	-- <> diverso
+	-- > maggiore
+	-- < minore
+	-- >= maggiore o uguale
+	-- <= minore o uguale
+	-- AND / OR
+order by -- 4
+	superficie_totale_ettari desc
