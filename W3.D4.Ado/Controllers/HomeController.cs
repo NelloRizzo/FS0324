f6666313d0d20ce3bcadd95e4584eb40d285ee0e
Nello Rizzo <nellorizzo@live.it>
2024-07-04 10:02:03 +0200

    Esecuzione di una SELECT

--- W3.D4.Ado/Controllers/HomeController.cs
+++ W3.D4.Ado/Controllers/HomeController.cs
@@ -16,6 +16,8 @@ namespace W3.D4.Ado.Controllers
         }
 
         public IActionResult Index() {
+            // modello dati da passare alla view
+            var list = new List<Customer>();
             // 1. Creazione di una connessione con il database
             // 1.1. Recupero la stringa di connessione
             var connStr = _config.GetConnectionString("AppConn");
@@ -23,12 +25,33 @@ namespace W3.D4.Ado.Controllers
             var conn = new SqlConnection(connStr);
             // per MySQL
             // var mysql = new MySqlConnector.MySqlConnection(connStr);
+            // 1.3. Apertura della connessione
+            conn.Open();
             // 2. Preparazione del comando di SELECT
+            var cmd = new SqlCommand("SELECT * FROM Customers", conn);
             // 3. Esecuzione del comando di SELECT con ottenimento del "cursore"
+            var reader = cmd.ExecuteReader();
             // 4. Attraversamento del cursore
-            // 5. Creazione di un Customer per ogni riga attraversata
+            while (reader.Read()) {
+                // 5. Creazione di un Customer per ogni riga attraversata
+                var customer = new Customer {
+                    Address = reader["Address"].ToString(),
+                    City = reader["City"].ToString(),
+                    CompanyName = reader["CompanyName"].ToString(),
+                    ContactName = reader["ContactName"].ToString(),
+                    ContactTitle = reader["ContactTitle"].ToString(),
+                    Country = reader["Country"].ToString(),
+                    CustomerID = reader["CustomerID"].ToString(),
+                    Fax = reader["Fax"].ToString(),
+                    Phone = reader["Phone"].ToString(),
+                    PostalCode = reader["PostalCode"].ToString(),
+                    Region = reader["Region"].ToString()
+                };
+                list.Add(customer);
+            }
             // 6. Chiusura della connessione
-            return View();
+            conn.Close();
+            return View(list);
         }
 
         public IActionResult Privacy() {
