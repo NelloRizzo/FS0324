ab65d901b1629756b6707a1307c8b1abf1f3aea9
Nello Rizzo <nellorizzo@live.it>
2024-07-10 16:21:02 +0200

    .

--- BuildWeek1/BusinessLayer/V1/ThumbnailService.cs
+++ BuildWeek1/BusinessLayer/V1/ThumbnailService.cs
@@ -19,22 +19,18 @@ namespace BuildWeek1.BusinessLayer.V1
         /// <param name="width">Larghezza desiderata.</param>
         /// <param name="height">Altezza desiderata.</param>
         /// <returns>La thumbnail dell'immagine alle dimensioni desiderate.</returns>
-        private byte[] ScaleImage(byte[] imageBytes, int? width, int? height)
-        {
+        private byte[] ScaleImage(byte[] imageBytes, int? width, int? height) {
             Image image = Image.Load(imageBytes);
-            lock (image)
-            {
+            lock (image) {
                 int newWidth = image.Width;
                 int newHeight = image.Height;
 
-                if (width.HasValue)
-                {
+                if (width.HasValue) {
                     var r = 1.0 * width.Value / image.Width;
                     newWidth = width.Value;
                     newHeight = (int)(image.Height * r);
                 }
-                if (height.HasValue)
-                {
+                if (height.HasValue) {
                     var r = 1.0 * height.Value / image.Height;
                     newHeight = height.Value;
                     newWidth = (int)(image.Width * r);
@@ -57,8 +53,7 @@ namespace BuildWeek1.BusinessLayer.V1
         /// dalla D.I. e passarli alla classe base per averli a disposizione da essa.</remarks>
         public ThumbnailService(DbContext context, ILogger<ThumbnailService> logger) : base(context, logger) { }
 
-        public byte[] Thumbnail(int imageId, int? width, int? height)
-        {
+        public byte[] Thumbnail(int imageId, int? width, int? height) {
             var image = _dbContext.Images.Read(imageId) ?? throw new EntityNotFoundException { Id = imageId, EntityType = typeof(ImageEntity) };
             return ScaleImage(image.Content, width, height);
         }
