eae1b5a0d22aac9a9bdc11f9ac21353738bc15f2
Nello Rizzo <nellorizzo@live.it>
2024-07-18 16:59:16 +0200

    Riorganizzazione del codice.

--- W7.D3.BusinessLayer/MailServices/DotNetMailService.cs
+++ W7.D3.BusinessLayer/MailServices/DotNetMailService.cs
@@ -1,19 +1,22 @@
 ﻿using System.Net;
 using System.Net.Mail;
 
-namespace W7.D3.BusinessLayer
+namespace W7.D3.BusinessLayer.MailServices
 {
     public class DotNetMailService : IMailService
     {
-        public void SendMail(string to, string subject, string body) {
-            using var client = new SmtpClient {
+        public void SendMail(string to, string subject, string body)
+        {
+            using var client = new SmtpClient
+            {
                 Host = "smtp.gmail.com",
                 EnableSsl = true,
                 DeliveryMethod = SmtpDeliveryMethod.Network,
                 UseDefaultCredentials = false,
                 Credentials = new NetworkCredential("nello.rizzo.test@gmail.com", "r1zz0nell0")
             };
-            using var msg = new MailMessage {
+            using var msg = new MailMessage
+            {
                 From = new MailAddress("nello.rizzo.test@gmail.com", "[My Web App] - Mail Sender"),
                 Subject = subject,
                 Body = body
--- W7.D3.BusinessLayer/MailServices/SendGridMailService.cs
+++ W7.D3.BusinessLayer/MailServices/SendGridMailService.cs
@@ -1,12 +1,14 @@
-﻿namespace W7.D3.BusinessLayer
+﻿namespace W7.D3.BusinessLayer.MailServices
 {
     public class SendGridMailService : IMailService
     {
         private readonly string apiKey;
-        public SendGridMailService(IConfiguration config) {
+        public SendGridMailService(IConfiguration config)
+        {
             apiKey = config["SendGrid"]!.ToString();
         }
-        public void SendMail(string to, string subject, string body) {
+        public void SendMail(string to, string subject, string body)
+        {
             var client = new SendGridClient(apiKey);
             var from = new EmailAddress("nellorizzo@live.it", "[My Web App]");
             var plainTextContent = body;
