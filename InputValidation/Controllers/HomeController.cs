e7e02c3bd51159eee77ce0a497ecf95c40131dca
Nello Rizzo <nellorizzo@live.it>
2024-07-22 10:59:46 +0200

    .

--- InputValidation/Controllers/HomeController.cs
+++ InputValidation/Controllers/HomeController.cs
@@ -16,6 +16,20 @@ namespace InputValidation.Controllers
             return View();
         }
 
+        public IActionResult FiscalCode() {
+            return View(new PersonalDataViewModel { BirthCity = "", FirstName = "", LastName = "", Gender = 'F' });
+        }
+
+        [HttpPost]
+        public IActionResult FiscalCode(PersonalDataViewModel model) {
+            // validazione lato server
+            if (ModelState.IsValid) {
+                ModelState.AddModelError("custom_validation_error", "Ho riscontrato un problema nei dati validandoli sul server");
+                ModelState.AddModelError(nameof(PersonalDataViewModel.BirthCity), "La città non esiste");
+            }
+            return View(model);
+        }
+
         public IActionResult Privacy() {
             return View();
         }
---
+++ InputValidation/Models/PersonalDataViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InputValidation.Models
+{
+    public class PersonalDataViewModel
+    {
+        [Display(Name = "Nome")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Immettere il nome")]
+        [StringLength(20, MinimumLength = 2, ErrorMessage = "Il nome deve contenere almeno 2 caratteri e non più di 20")]
+        public required string FirstName { get; set; }
+        [Display(Name = "Cognome")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Immettere il cognome")]
+        [StringLength(20, MinimumLength = 2, ErrorMessage = "Il cognome deve contenete almeno 2 caratteri e non più di 20")]
+        public required string LastName { get; set; }
+        [Display(Name = "Data di nascita")]
+        [Required(ErrorMessage = "Immettere la data di nascita")]
+        public DateOnly Birthday { get; set; }
+        [Display(Name = "Sesso")]
+        public char Gender { get; set; }
+        [Display(Name = "Città di nascita"), Required(ErrorMessage = "Immettere la città di nascita")]
+        public required string BirthCity { get; set; }
+    }
+}
